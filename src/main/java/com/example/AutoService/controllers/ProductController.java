bae2ddf480d7fad38e47213c627daa4cf1bdcdc7
Dbatr <bdanis2311@gmail.com>
2023-11-05 17:26:53 +0500

    test2

--- src/main/java/com/example/AutoService/controllers/ProductController.java
+++ src/main/java/com/example/AutoService/controllers/ProductController.java
@@ -1,12 +1,41 @@
 package com.example.AutoService.controllers;
 
+import com.example.AutoService.models.Product;
+import com.example.AutoService.services.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor
 public class ProductController {
+    private final ProductService productService;
+
     @GetMapping("/")
-    public String products() {
+    public String products(@RequestParam(name = "title", required = false) String title, Model model) {
+        model.addAttribute("products", productService.listProducts(title));
         return "products";
+    }
+
+    @GetMapping("/product/{id}")
+    public String productInfo(@PathVariable Long id, Model model) {
+        model.addAttribute("product", productService.getProductById(id));
+        return "product-info";
+    }
+
+    @PostMapping("/product/create")
+    public String createProduct(Product product) {
+        productService.saveProduct(product);
+        return "redirect:/";
+    }
+
+    @PostMapping("/product/delete/{id}")
+    public String deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return "redirect:/";
     }
 }
--- src/main/java/com/example/AutoService/models/Product.java
+++ src/main/java/com/example/AutoService/models/Product.java
@@ -1,4 +1,23 @@
 package com.example.AutoService.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "orders")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "description", columnDefinition = "text")
+    private String description;
 }
