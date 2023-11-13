2a7976a350f18c0e551964d278141755b8952e23
Dbatr <bdanis2311@gmail.com>
2023-11-13 15:50:18 +0500

    test2

--- src/main/java/com/example/AutoService/models/Product.java
+++ src/main/java/com/example/AutoService/models/Product.java
@@ -5,7 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
+//продукт с полями
 @Entity
 @Table(name = "orders")
 @Data
--- src/main/java/com/example/AutoService/services/ProductService.java
+++ src/main/java/com/example/AutoService/services/ProductService.java
@@ -5,30 +5,32 @@ import com.example.AutoService.repositories.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
 
+//методы для обработки бизнес-логики
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
-
+    //получение списка продуктов
     public List<Product> listProducts(String title) {
         if (title != null) return productRepository.findByTitle(title);
         return productRepository.findAll();
     }
 
+    // сохранение нового продукта
     public void saveProduct(Product product) {
         log.info("Saving new {}", product);
         productRepository.save(product);
     }
 
+    // удаление продукта
     public void deleteProduct(Long id) {
         productRepository.deleteById(id);
     }
 
+    // Получение доп. информации о продукте
     public Product getProductById(Long id) {
         return productRepository.findById(id).orElse(null);
     }
