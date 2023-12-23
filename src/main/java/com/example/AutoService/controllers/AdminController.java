9203dc86d4ffdf86234254694b75536693e7a93b
Dbatr <bdanis2311@gmail.com>
2023-12-23 20:02:15 +0500

    nine work

--- src/main/java/com/example/AutoService/controllers/AdminController.java
+++ src/main/java/com/example/AutoService/controllers/AdminController.java
@@ -1,11 +1,9 @@
 package com.example.AutoService.controllers;
 
-import com.example.AutoService.models.Mechanic;
 import com.example.AutoService.models.Product;
 import com.example.AutoService.services.MechanicService;
 import com.example.AutoService.services.ProductService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
